b1ff09ba42a5eb9103c85698da28219f05462ea0
unknown <callum.grimble18@gmail.com>
2023-02-22 21:08:31 +0000

    Initial Calendar Design

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+function CurrentDateTime() {
+  const [date, setDate] = useState(new Date());
+  setInterval(() => setDate(new Date()), 1000);
+
+  return <a className="dateTimeHolder">{date.toLocaleString().replace(',', '')}</a>
+}
+
+function Calendar() {
+  const today = new Date();
+  const [selectedDate, setSelectedDate] = useState(today);
+  return <div className="CalendarMain">
+    <CalendarNav selectedDate setSelectedDate />
+
+  </div>
+}
+
+function CalendarNav({setSelectedDate, selectedDate}) {
+  const currDate = new Date()
+  const daysInCurrentMonth = new Date(currDate.getFullYear(), currDate.getMonth()+1, 0).getDate();
+  return <div className="CalendarNav">
+    {[...new Array(daysInCurrentMonth)].map((_, i) => <div className="DateNavItem" data-date={i+1} />)}
+  </div>
+}
+
+function CalendarConfig() {
+  return <div className="CalendarConfig"></div>
+}
 
 function App() {
+
+  const date = new Date();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="CalendarHolder">
+        <h1 className="CalendarTitle">My {date.getFullYear()}</h1>
+        <h2 className="CalendarSubtitle">Today <CurrentDateTime /></h2>
+        <Calendar />
+        <CalendarConfig />
+      </div>
     </div>
   );
 }
